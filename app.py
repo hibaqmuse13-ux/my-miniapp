b87587d7160d366e3baceb87d25f6e3bf2487d69
hibaqmuse13-ux <hibaqmuse13@gmail.com>
2026-08-08 10:38:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -586,4 +586,4 @@ def webhook():
     return jsonify({"status": "ok"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
